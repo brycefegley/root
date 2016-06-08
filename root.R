136259503d2994188e01c7f1ed517f7ef470f40a
brycefegley <brycefegley@users.noreply.github.com>
2016-06-08 12:42:55 -0700

    Update root.R

--- root.R
+++ root.R
@@ -2,9 +2,9 @@ rm(list=ls())
 
 # Newton-Raphson general root finding algorithm for pth root of x^(1/p)
 # Let f(x) = x^p - S
-# Then x_(n+1) = x_n - f(x) / f'(x)
-# x_(n+1) = x_n - (x^p - S) / (p * x^(p-1))
-# x_(n+1) = (p-1)*x/p + S/(p * x^(p-1)) 
+# Then x_(n+1) = x_n - f(x_n) / f'(x_n)
+# x_(n+1) = x_n - (x_n^p - S) / (p * x_n^(p-1))
+# x_(n+1) = (p-1)*x_n/p + S/(p * x_n^(p-1)) 
 
 root <- function(S, p, x=1, tol=1e-7) {
   # S: number for which the ith root is sought
@@ -12,6 +12,7 @@ root <- function(S, p, x=1, tol=1e-7) {
   # x: starting guess
   
   i <- 2
+  # Also create an absolute value function for sake of it
   ABS <- function(x) if (x < 0) x * -1 else x
   x <- as.vector(x)
   x[i] <- (p-1) * x[i-1]/p + S/(p * x[i-1]^(p-1))
